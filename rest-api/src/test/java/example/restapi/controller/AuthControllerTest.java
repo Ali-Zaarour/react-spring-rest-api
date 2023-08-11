18c2513dc00c8cf73a15c7e84e80b096c9b02281
Ali Zaarour <zaarour.a@outlook.com>
2023-08-11 22:37:44 +0300

    feat(rest-api): test for signup method

---
+++ rest-api/src/test/java/example/restapi/controller/AuthControllerTest.java
@@ -0,0 +1,43 @@
+package example.restapi.controller;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import example.restapi.payload.SignupRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AuthControllerTest {
+
+    private final TestRestTemplate testRestTemplate;
+
+    @Autowired
+    public AuthControllerTest(TestRestTemplate testRestTemplate) {
+        this.testRestTemplate = testRestTemplate;
+    }
+
+    @Test
+    void shouldReturnAppUserDtoWhenUserIsCreated(){
+        var signupRequest = new SignupRequest("a@gmail.com","password");
+        ResponseEntity<String> createResponse = testRestTemplate.postForEntity("/uni-api/auth/signup",signupRequest,String.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(createResponse.getBody());
+        String username  = documentContext.read("$.username");
+        assertThat(username).isEqualTo("a@gmail.com");
+    }
+
+    @Test
+    void shouldReturnForbiddenWhenAnyOfRequestValueIsNotValid(){
+        var signupRequest = new SignupRequest("dc","");
+        ResponseEntity<String> createResponse = testRestTemplate.postForEntity("/uni-api/auth/signup",signupRequest,String.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
+}
